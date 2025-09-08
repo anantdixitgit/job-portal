055c0a9385f47d2060e8cbd24d2b5b192cb6d8f4
anant_dixit_13 <anantdixit100@gmail.com>
2025-09-08 10:21:52 +0530

    cookie response updated

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -103,7 +103,7 @@ export const login = async (req, res) => {
     const options = {
       maxAge: 1 * 24 * 60 * 60 * 1000,
       httpOnly: true,
-      sameSite: "strict",
+      secure: true,
     };
 
     return res
